6e19042eb73348dadf79578106caf717619207c1
Chocoerica <ho.ericat@gmail.com>
2019-09-22 01:08:56 -0700

    General cleanup, html changed to ejs and css file also works

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .engine('html', require('ejs').renderFile);
-  .set('view engine', 'html');
-  .get('/', (req, res) => res.render('calculator'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/calculator'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
