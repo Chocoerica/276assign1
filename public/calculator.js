ce8803c4fef30582a1057ec9bf15a4f433920797
Chocoerica <ho.ericat@gmail.com>
2019-09-22 22:23:12 -0700

    weight and mean now work, even with missing values

--- public/calculator.js
+++ public/calculator.js
@@ -32,26 +32,39 @@ document.getElementById("meanbutton").addEventListener('click', function () {
     }
     console.log("totals", total, "and length", percentages.length);
     //calculate mean by dividing
-    var mean = total / percentages.length;
+    var mean =(( total / percentages.length)*100).toFixed(3);
     console.log(mean);
-    document.getElementById("result").innerHTML = mean.toString();
+    document.getElementById("result").innerHTML = mean.toString() + "/100";
 })
 
 document.getElementById("weightbutton").addEventListener('click', function () {
     console.log("weight button clicked");
+    var x = document.getElementById("form1");
     var values = [];
-    for (var i = 0; i < x.length-2; i=i+3) {
-        if (!( isNaN(x.elements[i]) || isNaN(x.elements[i + 1])|| isNaN(x.elements[i + 2]))) { //remove inputs from form if they aren't filled out
-            values.push(x.elements[i]);
-            values.push(x.elements[i+1]);
-            values.push(x.elements[i+2]);
+    
+    for (var i = 0; i < x.length - 2; i = i + 3) {
+        console.log("i", i)
+        if (!((x.elements[i + 1].value.length == 0) || (x.elements[i + 2].value.length == 0))) { //remove inputs from form if they aren't filled out
+            values.push(parseInt(x.elements[i].value));
+            values.push(parseInt(x.elements[i + 1].value));
+            values.push(parseInt(x.elements[i + 2].value));
         }
     }
-
-    var weights = [];
-    for (var i = 0; i < x.length - 2; i = i + 3) { //get weights
-        weights += values.elements[i].value;
+    console.log(values);
+    console.log("now time for percentages with weights calculated");
+    //calculate total of percentages
+    var total = 0;
+    var totalweight = 0;
+    for (var i = 0; i < values.length - 2; i = i + 3) { //calculate sum of weight*num/den
+        total += values[i] * values[i + 1] / values[i + 2];
+        totalweight += values[i];
     }
-    console.log(weights);
-    document.getElementById("result").innerHTML = weights;
+    console.log("total",total);
+    console.log("total weight", totalweight);
+
+    //calculate mean with weights by dividing
+    var res= ((total / totalweight)*100).toFixed(3);
+
+    console.log(res);
+    document.getElementById("result").innerHTML = res.toString() + "/100";
 })
